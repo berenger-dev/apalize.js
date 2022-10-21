d3803d664f1a4dfb8960c6afdba9efbec7017327
Berenger.dev <hello@berenger.dev>
2022-10-21 16:32:38 +0200

    Init

--- src/index.ts
+++ src/index.ts
@@ -25,13 +25,12 @@ let translations: null | Translation[] = null;
 let missing_translations: Translation[] = [];
 
 const slugify = (text: string) => {
-    return text
-        .toString()
-        .toLowerCase()
-        .normalize('NFD')
-        .replace(/[\u0300-\u036f]/g, '')
-        .replace(/[^a-z0-9]+/g, '-')
-        .replace(/(^-|-$)+/g, '');
+    return text.toString().toLowerCase()
+        .replace(/\s+/g, '-')           // Replace spaces with -
+        .replace(/[^\w\-\.]+/g, '')     // Remove all non-word chars
+        .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+        .replace(/^-+/, '')             // Trim - from start of text
+        .replace(/-+$/, '');            // Trim - from end of text
 }
 
 const setLocale = (locale: string) => {
